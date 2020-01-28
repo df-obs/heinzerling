57e7869ee7771620cf75d8f12a1c8b86cd22daa0
David Faerber <david.faerber@odoo-bs.com>
2020-01-28 19:20:17 +0100

    [IMP] Enable Parse LocalDatastore and pin all objects on startup

--- app/src/main/java/com/example/aussendiensterfassung/ParseApplication.java
+++ app/src/main/java/com/example/aussendiensterfassung/ParseApplication.java
@@ -3,21 +3,47 @@ package com.example.aussendiensterfassung;
 import android.app.Application;
 import com.parse.*;
 
+import java.util.List;
+
 public class ParseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        Parse.enableLocalDatastore(this);
-
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.parse_app_id))
+                .enableLocalDataStore()
                 .clientKey(getString(R.string.parse_client_key))
                 .server(getString(R.string.parse_server_url))
                 .build()
         );
 
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
+        try {
+            ParseObject.unpinAll();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        String[] tables = {"Ansprechpartner", "Artikel", "ArtikelAuftrag", "Auftrag", "Aufzug", "Einzelauftrag", "Kunde", "Monteur", "MonteurAuftrag"};
+
+        for (int i=0; i<tables.length; i++) {
+            pinData(tables[i]);
+        }
+    }
+
+    public void pinData(String table) {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery(table);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> resultList, ParseException e) {
+                if (e == null) {
+                    try {
+                        ParseObject.pinAll(resultList);
+                    } catch (ParseException ex) {
+                        ex.printStackTrace();
+                    }
+                } else {
+                    // error
+                }
+            }
+        });
     }
 }
